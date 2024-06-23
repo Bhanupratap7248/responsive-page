472c0c00a35c522f353389180612e837c77e3766
Bhanu pratap <142300187+Bhanupratap7248@users.noreply.github.com>
2024-06-23 12:13:42 +0530

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,10 @@
+// scripts.js
+
+window.addEventListener('scroll', function() {
+    const navbar = document.getElementById('navbar');
+    if (window.scrollY > 50) {
+        navbar.style.backgroundColor = '#222';
+    } else {
+        navbar.style.backgroundColor = '#333';
+    }
+});
